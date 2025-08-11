9519a11fd90729a6c8f56765180278f40b09e399
vadim <kazakovvadim64@gmail.com>
2025-08-11 17:25:24 +0400

    fix

--- service_a/src/main/java/mentorship/roadmap/microservices/service_a/model/dto/MessageDto.java
+++ service_a/src/main/java/mentorship/roadmap/microservices/service_a/model/dto/MessageDto.java
@@ -1,10 +1,14 @@
 package mentorship.roadmap.microservices.service_a.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto {
     private String key;
     private String type;
